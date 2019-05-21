e907df698133cbada2992866b00193a11b7a7a94
Austin Hill <austin.hill42@gmail.com>
2019-05-21 14:28:45 -0500

    Split CSV

--- CSV Converter/CSV Converter/CSV Converter.cs
+++ CSV Converter/CSV Converter/CSV Converter.cs
@@ -20,6 +20,9 @@ namespace CSV_Converter
         public CSVConverter()
         {
             InitializeComponent();
+
+            L_InputFilename.Text = "";
+            L_OutputFileName.Text = "";
         }
 
         private void BTN_InputFileSelect_Click(object sender, EventArgs e)
@@ -28,22 +31,32 @@ namespace CSV_Converter
 
             if (openFile.ShowDialog() == DialogResult.OK)
             {
-                try
-                {
-                    StreamReader sr = new StreamReader(openFile.FileName);
-
-                    L_InputFilename.Text = sr.ReadToEnd();
-                }
-                catch (SecurityException ex)
-                {
-                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\nDetails:\n\n{ex.StackTrace}");
-                }
+                List<string> str = openFile.FileName.Split('.').ToList();
+                str.Insert(1, "(converted).");
+                
+                L_InputFilename.Text = openFile.FileName;
+                L_OutputFileName.Text = String.Join("", str);
             }
         }
 
         private void BTN_Convert_Click(object sender, EventArgs e)
         {
+            using (StreamReader sr = new StreamReader(openFile.FileName))
+            {
+                List<List<string>> csv = new List<List<string>>();
 
+                string str = sr.ReadToEnd();
+                str = str.Replace("\"", "");
+
+                List<string> lst = str.Split('\n').ToList();
+                lst.RemoveRange(0, 4);
+                
+                foreach (string l in lst)
+                {
+                    csv.Add(l.Split(',').ToList());
+                }
+
+            }
         }
     }
 }
