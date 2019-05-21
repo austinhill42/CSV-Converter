0d6719719a409c00a40d827e688ccc986fb7c514
Austin Hill <austin.hill42@gmail.com>
2019-05-21 13:53:39 -0500

    Open file dialog added

--- CSV Converter/CSV Converter/CSV Converter.Designer.cs
+++ CSV Converter/CSV Converter/CSV Converter.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CSV_Converter
 {
-    partial class Form1
+    partial class CSVConverter
     {
         /// <summary>
         /// Required designer variable.
@@ -30,6 +30,8 @@
         {
             this.BTN_InputFileSelect = new System.Windows.Forms.Button();
             this.L_InputFilename = new System.Windows.Forms.Label();
+            this.L_OutputFileName = new System.Windows.Forms.Label();
+            this.BTN_Convert = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // BTN_InputFileSelect
@@ -41,6 +43,7 @@
             this.BTN_InputFileSelect.TabIndex = 0;
             this.BTN_InputFileSelect.Text = "Select File:";
             this.BTN_InputFileSelect.UseVisualStyleBackColor = true;
+            this.BTN_InputFileSelect.Click += new System.EventHandler(this.BTN_InputFileSelect_Click);
             // 
             // L_InputFilename
             // 
@@ -48,19 +51,45 @@
             this.L_InputFilename.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.L_InputFilename.Location = new System.Drawing.Point(161, 69);
             this.L_InputFilename.Name = "L_InputFilename";
-            this.L_InputFilename.Size = new System.Drawing.Size(0, 24);
+            this.L_InputFilename.Size = new System.Drawing.Size(133, 24);
             this.L_InputFilename.TabIndex = 1;
+            this.L_InputFilename.Text = "InputFileName";
+            // 
+            // L_OutputFileName
+            // 
+            this.L_OutputFileName.AutoSize = true;
+            this.L_OutputFileName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.L_OutputFileName.Location = new System.Drawing.Point(161, 135);
+            this.L_OutputFileName.Name = "L_OutputFileName";
+            this.L_OutputFileName.Size = new System.Drawing.Size(148, 24);
+            this.L_OutputFileName.TabIndex = 2;
+            this.L_OutputFileName.Text = "OutputFileName";
+            // 
+            // BTN_Convert
+            // 
+            this.BTN_Convert.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_Convert.Location = new System.Drawing.Point(652, 399);
+            this.BTN_Convert.Name = "BTN_Convert";
+            this.BTN_Convert.Size = new System.Drawing.Size(120, 51);
+            this.BTN_Convert.TabIndex = 3;
+            this.BTN_Convert.Text = "Convert";
+            this.BTN_Convert.UseVisualStyleBackColor = true;
+            this.BTN_Convert.Click += new System.EventHandler(this.BTN_Convert_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(784, 462);
+            this.Controls.Add(this.BTN_Convert);
+            this.Controls.Add(this.L_OutputFileName);
             this.Controls.Add(this.L_InputFilename);
             this.Controls.Add(this.BTN_InputFileSelect);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 500);
+            this.MinimumSize = new System.Drawing.Size(800, 500);
             this.Name = "Form1";
             this.Text = "CSV Converter";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -70,6 +99,8 @@
 
         private System.Windows.Forms.Button BTN_InputFileSelect;
         private System.Windows.Forms.Label L_InputFilename;
+        private System.Windows.Forms.Label L_OutputFileName;
+        private System.Windows.Forms.Button BTN_Convert;
     }
 }
 
--- CSV Converter/CSV Converter/CSV Converter.cs
+++ CSV Converter/CSV Converter/CSV Converter.cs
@@ -3,21 +3,45 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CSV_Converter
 {
-    public partial class Form1 : Form
+    public partial class CSVConverter : Form
     {
-        public Form1()
+
+        private OpenFileDialog openFile;
+
+        public CSVConverter()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void BTN_InputFileSelect_Click(object sender, EventArgs e)
+        {
+            openFile = new OpenFileDialog();
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    StreamReader sr = new StreamReader(openFile.FileName);
+
+                    L_InputFilename.Text = sr.ReadToEnd();
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\nDetails:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+
+        private void BTN_Convert_Click(object sender, EventArgs e)
         {
 
         }
